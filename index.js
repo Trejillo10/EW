b6a81198ce72ed95a982f99098865f45f42383d1
Trejillo10 <gachubela10@gmail.com>
2022-11-08 00:45:19 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
+const bodyParser =require('body-parser');
 const express = require('express');
 const app = express();
+const {pokemon} =require('./pokedex.json');
 
-app.get("/", (req, res, next) =>{
-    res.status(200);
-    res.send("Bienvenido")
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
+app.get("/", (req, res, next) => {
+    return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.listen(3000,() => {
-    console.log("Hola Mundo");
+app.post('/pokemon', (req, res, next) =>{
+    return res.status(200).send(req.body.name);
+})
+
+app.get("/pokemon", (req, res, next) => {
+    return res.status(200).send(pokemon);
 });
+
+app.get('/pokemon/:id([0-9]{1,3})', (req,res,next) => {
+    const id = req.params.id-1;
+    if(id >=0 && id <= 150) {
+        return res.status(200).send(pokemon[req.params.id -1]);
+    }
+    res.status(404).send("Pokémon no encontrado");
+});
+
+app.get('/pokemon/:name([A-Za-z]+)', (req, res,) => {
+    const name = req.params.name;
+
+    const pk = pokemon.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase ()) && p;
+    });
+
+   if(pk.length > 0) {
+        res.status(200).send(pk);
+    } 
+        return res.status(404).send("Pokémon no encontrado");
+});
+
+   app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running");
+});
+
 
 
 /*
+USE: se usa cuando queremos que alguna función se le aplque a 
+todas las peticiones que entren al servidor
+Sirve para añadir Middlewares (capas a nuestra aplicación; 
+capas que procesarán las peticiones y que les harán alguna 
+modificación o revisar algún dato.)
+
 verbos HTTP
 GET --> Lo que ejecuta nuestros navegadores cada
 que ponemos una url. Regresa una página web
+- OBTENER RECURSOS (imagen, bd, etc)
 
 POST --> Nos sirve para guardar o publicar algo.
 Como cuando nos registramos en algún sitio web
+-ALMACENAR/CREAR RECURSOS
 
 PATCH --> Actualización de UN dato de un recurso 
 (registro en una bd) en específico  
+-MODIFICAR UNA PARTE DE UN RECURSO 
 
 PUT --> Modifica TODOS los elementos 
 Programar para que se haga
+-MODIFICAR UN RECURSO COMPLETO
 
 DELETE --> Eliminiar un recurso
 
@@ -34,4 +77,52 @@ DELETE --> Eliminiar un recurso
 req -> petición del cliente (navegador)
 res -> objeto que nos ayuda a contestar
 la petición 
+*/
+
+
+
+
+/*for(i=0; i< pokemon.length; i++) {
+        if(pokemon[i].name.toUpperCase() == name.toUpperCase()) {
+            return res.status(200).send(pokemon[i]);
+        }
+    }*/
+
+
+/*OPERADOR TERNARIO condicion ? valor si verdadero : valor si falso (es un if)
+EJEMPLO: 
+app.get('/pokemon/:id([0-9]{1,3})', (req,res,next) => {
+    const id = req.params.id-1;
+    if(id >=0 && id <= 150) {
+       return res.status(200).send(pokemon[req.params.id -1]);
+    }
+    return res.status(404).send("Pokémon no encontrado");
+});
+LO DEL IF SERÍA:
+app.get('/pokemon/:id([0-9]{1,3})', (req,res,next) => {
+    const id = req.params.id-1;
+    (id >=0 && id <= 150) ? 
+    res.status(200).send(pokemon[req.params.id -1]):
+    res.status(404).send("Pokémon no encontrado");
+});
+
+
+app.get('/pokemon/:name([A-Za-z]+)', (req, res,) => {
+
+    const name = req.params.name;
+    const pk = pokemon.filter((p) => {
+        if(p.name.toUpperCase() == name.toUpperCase()){
+            return p;
+        }
+    });
+
+app.get('/pokemon/:name([A-Za-z]+)', (req, res,) => {
+    const name = req.params.name;
+    const pk = pokemon.filter((p) => {
+        (p.name.toUpperCase() == name.toUpperCase ()) ? p : null;
+    });
+
+   (pk.length > 0) ? res.status(200).send(pk) : res.status(404).send("Pokémon no encontrado");
+});
+
 */
